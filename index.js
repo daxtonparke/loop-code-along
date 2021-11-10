38bb45342ac1d9f16d8b0e5ad01ce406e4e7c6af
daxtonparke <daxtonparke@gmail.com>
2021-11-10 13:30:07 -0700

    complete

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log('its working')
+
+let jonsnowattack = 25
+let jamielannisterattack = 35
+
+let jonsnowhealth = 50
+let jamielannisterhealth = 50
+
+let jonsnowdefence = 0
+let jamielannister = 20
+
+// if (jonsnowattack<jamielannisterattack){
+//     console.log('jamie has the stronger attack')
+// } else if (jonsnowattack>jamielannisterattack){
+// console.log('jon has the stronger attack')
+// } else console.log('the attacks are equally powerfull')
+
+jonsnowdefence += 25
+
+// for(let i=0; i>5; i++){
+// if (jonsnowhealth<=0){
+//     console.log('jonsnow has perished')
+// } else {
+//     jonsnowhealth -= (jamielannisterattack - jonsnowdefence)
+//     console.log (`jons health is ${jonsnowhealth}`)
+// } }
+
+for (let i=10; i > 0; i --){
+    console.log(i)
+}
